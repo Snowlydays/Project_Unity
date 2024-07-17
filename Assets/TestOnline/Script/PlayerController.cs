2eff41b1024f4a81bec6e37002c605d85884d629
tamura <tomoki0927.7939@icloud.com>
2024-07-17 14:37:03 +0900

    通信テスト 環境構築完了

--- Assets/TestOnline/Script/PlayerController.cs
+++ Assets/TestOnline/Script/PlayerController.cs
@@ -3,29 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 using Random = UnityEngine.Random;
-using UnityEngine.UI;
 using TMPro;
 
 public class PlayerController : NetworkBehaviour
 {
     private NetworkList<int> HostNetworkData;
     private NetworkList<int> ClientNetworkData;
-    //[SerializeField]
-    //private GameObject textobj;
-    //GameObject clonetextobj;
-    //public GameObject clienttext = GameObject.Find("TextC")
+    [SerializeField]
+    private string hostTextObjectName = "HostText"; // ホスト用TextMeshProオブジェクトの名前
+    private TextMeshProUGUI hostText;
+
+    [SerializeField]
+    private string clientTextObjectName = "ClientText"; // クライアント用TextMeshProオブジェクトの名前
+    private TextMeshProUGUI clientText;
+
+
     void Awake()
     {
         HostNetworkData = new NetworkList<int>();
         ClientNetworkData = new NetworkList<int>();
-
-
     }
+
+    public override void OnNetworkSpawn()
+    {
+        // NetworkListの初期化と変更イベントの登録
+        HostNetworkData.OnListChanged += OnHostNetworkDataChanged;
+        ClientNetworkData.OnListChanged += OnClientNetworkDataChanged;
+    }
+
+    private void OnHostNetworkDataChanged(NetworkListEvent<int> changeEvent)
+    {
+        Debug.Log("HostNetworkData changed:");
+        UpdateHostText();
+    }
+
+    private void OnClientNetworkDataChanged(NetworkListEvent<int> changeEvent)
+    {
+        Debug.Log("ClientNetworkData changed:");
+        UpdateClientText();
+    }
+
     void ShuffleCards(NetworkList<int> array)
     {
         for (int i = array.Count - 1; i > 0; i--)
         {
-            var j = Random.Range(0, i + 1); // UnityEnginのRandomを使用
+            var j = Random.Range(0, i + 1); // UnityEngineのRandomを使用
             (array[i], array[j]) = (array[j], array[i]); // スワップ
         }
         for (int i = 0; i < array.Count; i++)
@@ -33,15 +55,18 @@ public class PlayerController : NetworkBehaviour
             Debug.Log(array[i]);
         }
     }
-    void ClientShuffleCards(){
+
+    void ClientShuffleCards()
+    {
         ClientShuffleCardsServerRpc();
     }
+
     [Unity.Netcode.ServerRpc(RequireOwnership = false)]
     void ClientShuffleCardsServerRpc()
     {
         for (int i = ClientNetworkData.Count - 1; i > 0; i--)
         {
-            var j = Random.Range(0, i + 1); // UnityEnginのRandomを使用
+            var j = Random.Range(0, i + 1); // UnityEngineのRandomを使用
             (ClientNetworkData[i], ClientNetworkData[j]) = (ClientNetworkData[j], ClientNetworkData[i]); // スワップ
         }
         for (int i = 0; i < ClientNetworkData.Count; i++)
@@ -56,7 +81,7 @@ public class PlayerController : NetworkBehaviour
         ClientNetworkData?.Dispose();
     }
 
-    void AddListtoCard(List<int> list,NetworkList<int> array)
+    void AddListtoCard(List<int> list, NetworkList<int> array)
     {
         for (int i = 0; i < array.Count; i++)
         {
@@ -64,11 +89,11 @@ public class PlayerController : NetworkBehaviour
         }
     }
 
-    void SetListtoCard(List<int> list,NetworkList<int> array)
+    void SetListtoCard(List<int> list, NetworkList<int> array)
     {
         for (int i = 0; i < array.Count; i++)
         {
-            list[i]=array[i];
+            list[i] = array[i];
         }
     }
 
@@ -78,108 +103,78 @@ public class PlayerController : NetworkBehaviour
     {
         //hosttext = GameObject.Find("TextH");
         //clienttext = GameObject.Find("TextC");
-        
+
         //GameObject[] objects = Resources.LoadAll<GameObject>("Cards");//フォルダCardsにあるプレファブ済みオブジェクトを配列取得
 
-        if(IsHost)
+        GameObject hostTextObject = GameObject.Find(hostTextObjectName);
+            if (hostTextObject != null)
+                hostText = hostTextObject.GetComponent<TextMeshProUGUI>();
+
+            GameObject clientTextObject = GameObject.Find(clientTextObjectName);
+            if (clientTextObject != null)
+                clientText = clientTextObject.GetComponent<TextMeshProUGUI>();
+
+        if (IsHost)
         {
-            //clonetextobj = Instantiate(textobj,new Vector3(-344,769,0),Quaternion.identity);
-            for (int i = 0; i < objects.Length; i++)//カードデータ用意
+            //clonetextobj = Instantiate(textobj, new Vector3(-344, 769, 0), Quaternion.identity);
+            for (int i = 0; i < 3; i++) //カードデータ用意
             {
-                int cardNum = i+1;
+                int cardNum = i + 1;
                 HostNetworkData.Add(cardNum);
             }
             ShuffleCards(HostNetworkData);
-            //AddListtoCard(CardController.HostCardList,HostNetworkData);
-            /*for(int i=0;i<HostNetworkData.Count;i++)//ゲーム画面上に生成
-            {
-                GameObject instance = Instantiate(objects[HostNetworkData[i]-1],
-                    new Vector3(-1.2f+1.2f*i,-2.0f,0.0f),
-                    Quaternion.identity);
-                MyCards[i]=instance;
-                var networkObj = instance.GetComponent<NetworkObject>();
-                networkObj.Spawn(true);
-            }*/
 
-            for (int i = 0; i < objects.Length; i++)//カードデータ用意
+            for (int i = 0; i < 3; i++) //カードデータ用意
             {
-                int cardNum = i+1;
+                int cardNum = i + 1;
                 ClientNetworkData.Add(cardNum);
             }
-            ShuffleCards(ClientNetworkData);//並び替え
-            //AddListtoCard(CardController.ClientCardList,ClientNetworkData);
-            /*HostNetworkData.OnListChanged += (NetworkListEvent<int> changed) =>
-            {
-                    Debug.Log(string.Join(",", changed.Value));
-            };
-            ClientNetworkData.OnListChanged += (NetworkListEvent<int> changed) =>
-            {
-                    Debug.Log(string.Join(",", changed.Value));
-            };*/
-        }else{
-            /*for(int i=0;i<ClientNetworkData.Count;i++)//ゲーム画面上に生成
-            {
-                GameObject instance = Instantiate(objects[ClientNetworkData[i]-1],
-                    new Vector3(-1.2f+1.2f*i,2.0f,0.0f),
-                    Quaternion.identity);
-                MyCards[i]=instance;
-                var networkObj = instance.GetComponent<NetworkObject>();
-                networkObj.Spawn(true);
-            }*/
-            /*for (int i = 0; i < objects.Length; i++)//カードデータ用意
-            {
-                int cardNum = int.Parse(objects[i].name.Substring(5));
-                ClientNetworkData.Value[i] = cardNum;
-            }
-            for (int i = 0; i < ClientNetworkData.Value.Length; i++)Debug.Log(ClientNetworkData.Value[i]);
-            ShuffleCards(ClientNetworkData.Value);//並び替え
-            for (int i = 0; i < ClientNetworkData.Value.Length; i++)Debug.Log(i.ToString()+ " "+ClientNetworkData.Value[i].ToString());
-            for(int i=0;i<ClientNetworkData.Value.Length;i++)//ゲーム画面上に生成
-            {
-                ClientCards[i].Add(
-                    Instantiate(objects[ClientNetworkData.Value[i]-1],
-                    new Vector3(-1.2f+1.2f*i,-2.0f,0.0f),
-                    Quaternion.identity);
-                );
-            }*/
+            ShuffleCards(ClientNetworkData); //並び替え
+
+            UpdateHostText();
+            UpdateClientText();
         }
     }
+
     private void Update()
     {
-        
         if (IsOwner == false)
         {
             return;
         }
 
-        if(IsHost)
+        if (IsHost)
         {
-            if(Input.GetKeyDown(KeyCode.UpArrow)){
-                ShuffleCards(HostNetworkData);//並び替え
-                //SetListtoCard(CardController.HostCardList,HostNetworkData);
-                /*for (int i = 0; i < HostNetworkData.Count; i++)
-                {
-                    Vector3 pos = new Vector3(-1.2f+1.2f*(HostNetworkData[i]-1),-2.0f,0.0f);
-                    MyCards[i].transform.position = pos;
-                }*/
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                ShuffleCards(HostNetworkData); //並び替え
+                UpdateHostText();
             }
-            /*clonetextobj.GetComponent<TextMeshProUGUI>().text ="";
-            if(HostNetworkData?.Count>0){
-                for (int i = 0; i < HostNetworkData.Count; i++)
-                {
-                    clonetextobj.GetComponent<TextMeshProUGUI>().text +=HostNetworkData[i].ToString() + " ";
-                }
-            }*/
-        }else{
-            if(Input.GetKeyDown(KeyCode.UpArrow)){
-                ClientShuffleCards();//並び替え
-                //SetListtoCard(CardController.ClientCardList,ClientNetworkData);
-                /*for (int i = 0; i < ClientNetworkData.Count; i++)
-                {
-                    Vector3 pos = new Vector3(-1.2f+1.2f*(ClientNetworkData[i]-1),2.0f,0.0f);
-                    MyCards[i].transform.position = pos;
-                }*/
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                ClientShuffleCards(); //並び替え
             }
+        }
+    }
+
+    private void UpdateHostText()
+    {
+        hostText.text = "Host Network Data: ";
+        foreach (var item in HostNetworkData)
+        {
+            hostText.text += item.ToString() + " ";
+        }
+    }
+
+    private void UpdateClientText()
+    {
+        clientText.text = "Client Network Data: ";
+        foreach (var item in ClientNetworkData)
+        {
+            clientText.text += item.ToString() + " ";
         }
     }
 }
