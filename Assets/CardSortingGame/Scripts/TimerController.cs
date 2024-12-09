9151f6e3b023f86675a1df575209a6f16dea407f
weewonwe <Humwestfree@gmail.com>
2024-12-09 14:28:21 +0900

    残り時間表示

--- Assets/CardSortingGame/Scripts/TimerController.cs
+++ Assets/CardSortingGame/Scripts/TimerController.cs
@@ -6,17 +6,19 @@ using System;
 using UnityEditor;
 using Unity.Collections;
 using Random = UnityEngine.Random;
+using TMPro;
 
 public class TimerController : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject circleObject,readyButton; // 制限時間バー
+    public GameObject circleObject,readyButton,textObj;
     Image circleImage,buttonImage;
     public bool isCountNow=false;
     float H,S,V,nowTime,buttonX,buttonY;
     public float timerSec=60f;//計測する制限時間(秒)
     [SerializeField] private Sprite CountingSprite;
     [SerializeField] private Sprite notCountingSprite;
+    private TextMeshProUGUI countText;
 
     NetworkSystem networkSystem;
 
@@ -27,7 +29,9 @@ public class TimerController : MonoBehaviour
         buttonX=readyButton.GetComponent<RectTransform>().anchoredPosition.x;
         buttonY=readyButton.GetComponent<RectTransform>().anchoredPosition.y;
         circleImage=circleObject.GetComponent<Image>();
+        countText=textObj.GetComponent<TextMeshProUGUI>();
         circleObject.SetActive(false);
+        textObj.SetActive(false);
         Color.RGBToHSV(circleImage.color, out H, out S, out V);
         StartDrawBar();
     }
@@ -43,6 +47,7 @@ public class TimerController : MonoBehaviour
                 0f
             );
             nowTime-=1f/50f;
+            countText.text=Mathf.Ceil(nowTime).ToString();
             if(nowTime<=0f){
                 networkSystem.mainSystemScript.OnReadyButtonClicked();
                 isCountNow=false;
@@ -54,6 +59,7 @@ public class TimerController : MonoBehaviour
         isCountNow=true;
         nowTime=timerSec;
         circleObject.SetActive(true);
+        textObj.SetActive(true);
         buttonImage.sprite=CountingSprite;
         readyButton.GetComponent<Button>().enabled=true;
     }
@@ -63,6 +69,7 @@ public class TimerController : MonoBehaviour
         nowTime=0f;
         buttonImage.sprite=notCountingSprite;
         circleObject.SetActive(false);
+        textObj.SetActive(false);
         readyButton.GetComponent<RectTransform>().anchoredPosition=new Vector3(buttonX,buttonY,0f);
         readyButton.GetComponent<Button>().enabled=false;
         
